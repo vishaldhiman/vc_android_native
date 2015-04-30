a4584392a54a763866ae2f0a0d4355d770650473
Archie <agrawal.archie@gmail.com>
2015-04-30 15:19:38 -0400

    Privacy Policy added

--- app/src/main/java/com/vchoose/Vchoose/MainPagerActivity.java
+++ app/src/main/java/com/vchoose/Vchoose/MainPagerActivity.java
@@ -4,6 +4,7 @@ package com.vchoose.Vchoose;
 * */
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.app.ProgressDialog;
 import android.content.Context;
@@ -28,8 +29,10 @@ import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.util.Pair;
+import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -81,12 +84,13 @@ public class MainPagerActivity extends FragmentActivity implements GoogleApiClie
     Location mLastLocation;
     GoogleApiClient mGoogleApiClient;
     Spinner spinner;
-    Button searchButton;
+    ImageButton searchButton;
     ArrayList<String> hint = new ArrayList<String>();
     ArrayList<HashMap<String, String>> dishJsonlist = new ArrayList<HashMap<String, String>>();
     ArrayList<HashMap<String, String>> restaurantJsonlist = new ArrayList<HashMap<String, String>>();
     ArrayList<Pair<String, LatLng>> mapMarkers = new ArrayList<Pair<String, LatLng>>();
 
+    private TextView linkTextview;
 
     /**
      * The {@link ViewPager} that will host the section contents.
@@ -102,9 +106,16 @@ public class MainPagerActivity extends FragmentActivity implements GoogleApiClie
         mEdit   = (AutoCompleteTextView)findViewById(R.id.editText);
         locationEdit = (EditText)findViewById(R.id.editTextLocation);
         spinner = (Spinner) findViewById(R.id.spinner);
-        searchButton = (Button)findViewById(R.id.searchButton);
+        searchButton = (ImageButton)findViewById(R.id.searchButton);
         context = this;
 
+        linkTextview = (TextView) findViewById(R.id.txtLink);
+        linkTextview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createDialog();
+            }
+        });
         mEdit.addTextChangedListener(new InputValidator());
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(context,
@@ -127,6 +138,43 @@ public class MainPagerActivity extends FragmentActivity implements GoogleApiClie
         // Set up the ViewPager with the sections adapter.
     }
 
+    private void createDialog()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Privacy Policy");
+        builder.setMessage("PRIVACY POLICY\n" +
+                "This privacy policy governs your use of the software application vChoose (“Application”) for mobile devices that was created by . The Application is search engine for dishes which also provides personalized food recommendations to the users.\n" +
+                "What information does the Application obtain and how is it used?\n" +
+                "User Provided Information\n" +
+                "The Application obtains the information you provide when you download and register the Application. Registration with us is optional. However, please keep in mind that you may not be able to use some of the features offered by the Application unless you register with us.\n" +
+                "When you register with us and use the Application, you generally or may provide (a) your name, email address, age, user name, password and other registration information; (b) transaction-related information, such as when you make purchases, respond to any offers, or download or use applications from us; (c) information you provide us when you contact us for help; (d) credit card information for purchase and use of the Application, and; (e) information you enter into our system when using the Application, such as contact information and project management information.\n" +
+                "We may also use the information you provided us to contact your from time to time to provide you with important information, required notices and marketing promotions.\n" +
+                "Automatically Collected Information\n" +
+                "In addition, the Application may collect certain information automatically, including, but not limited to, the type of mobile device you use, your mobile devices unique device ID, the IP address of your mobile device, your mobile operating system, the type of mobile Internet browsers you use, and information about the way you use the Application.\n" +
+                "When you visit the mobile application, we use GPS technology (or other similar technology) to determine your current location in order to determine the city you are located within and display a location map with relevant advertisements. We will not share your current location with other users or partners.\n" +
+                "If you do not want us to use your location for the purposes set forth above, you should turn off the location services for the mobile application located in your account settings or in your mobile phone settings and/or within the mobile application.\n" +
+                "Do third parties see and/or have access to information obtained by the Application?\n" +
+                "Yes. We will share your information with third parties only in the ways that are described in this privacy statement.\n" +
+                "We may disclose User Provided and Automatically Collected Information:\n" +
+                "   \n" +
+                "·\tas required by law, such as to comply with a subpoena, or similar legal process;\n" +
+                "·\twhen we believe in good faith that disclosure is necessary to protect our rights, protect your safety or the safety of others, investigate fraud, or respond to a government request;\n" +
+                "·\twith our trusted services providers who work on our behalf, do not have an independent use of the information we disclose to them, and have agreed to adhere to the rules set forth in this privacy statement.\n" +
+                "·\tif is involved in a merger, acquisition, or sale of all or a portion of its assets, you will be notified via email and/or a prominent notice on our Web site of any change in ownership or uses of this information, as well as any choices you may have regarding this information.\n" +
+                "What are my opt-out rights?\n" +
+                "You can stop all collection of information by the Application easily by uninstalling the Application. You may use the standard uninstall processes as may be available as part of your mobile device or via the mobile application marketplace or network. You can also request to opt-out via email.\n" +
+                "Data Retention Policy, Managing Your Information\n" +
+                "We will retain User Provided data for as long as you use the Application and for a reasonable time thereafter. We will retain Automatically Collected information for up to 24 months and thereafter may store it in aggregate. If you’d like us to delete User Provided Data that you have provided via the Application, please contact us at and we will respond in a reasonable time. Please note that some or all of the User Provided Data may be required in order for the Application to function properly.\n" +
+                "Security\n" +
+                "We are concerned about safeguarding the confidentiality of your information. We provide physical, electronic, and procedural safeguards to protect information we process and maintain. For example, we limit access to this information to authorized employees and contractors who need to know that information in order to operate, develop or improve our Application. Please be aware that, although we endeavor provide reasonable security for information we process and maintain, no security system can prevent all potential security breaches.\n" +
+                "Changes\n" +
+                "This Privacy Policy may be updated from time to time for any reason. We will notify you of any changes to our Privacy Policy by posting the new Privacy Policy here and informing you via email or text message. You are advised to consult this Privacy Policy regularly for any changes, as continued use is deemed approval of all changes.\n" +
+                "Your Consent\n" +
+                "By using the Application, you are consenting to our processing of your information as set forth in this Privacy Policy now and as amended by us. \"Processing,” means using cookies on a computer/hand held device or using or touching information in any way, including, but not limited to, collecting, storing, deleting, using, combining and disclosing information, all of which activities will take place in the United States. If you reside outside the United States your information will be transferred, processed and stored there under United States privacy standards.\n" +
+                "Contact us\n" +
+                "If you have any questions regarding privacy while using the Application, or have questions about our practices, please contact us via email at support@vchoose.us.\n");
+        builder.create().show();
+    }
     protected synchronized void buildGoogleApiClient() {
         mGoogleApiClient = new GoogleApiClient.Builder(context)
                 .addConnectionCallbacks(this)
@@ -572,4 +620,5 @@ public class MainPagerActivity extends FragmentActivity implements GoogleApiClie
             Log.v("AuthenticationToken", this.AuthenticationToken);
         }
     }
+
 }
