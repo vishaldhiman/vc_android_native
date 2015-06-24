991d55b8b1b0bf429bf5e2630f1575970faa3ce5
xamguo <guoxiao113113@gmail.com>
2015-06-24 10:24:21 -0700

    function modify 0.2

--- app/src/main/java/com/vchoose/Vchoose/Login.java
+++ app/src/main/java/com/vchoose/Vchoose/Login.java
@@ -1,22 +1,45 @@
 package com.vchoose.Vchoose;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Base64;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.vchoose.Vchoose.R;
 import com.vchoose.Vchoose.util.VcJsonReader;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import static android.content.pm.PackageManager.NameNotFoundException;
 
 public class Login extends ActionBarActivity {
 
@@ -27,10 +50,53 @@ public class Login extends ActionBarActivity {
 
     String auth_token;
 
+    LoginButton loginButton;
+    CallbackManager callbackManager;
+
+    final String TAG = "XiaoGuoTest_";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+
         setContentView(R.layout.activity_login);
+
+
+        loginButton = (LoginButton) this.findViewById(R.id.facebook_login);
+        loginButton.setReadPermissions("user_friends");
+
+        AccessToken a = AccessToken.getCurrentAccessToken();
+        if(a == null){
+            Log.v(TAG+"myToken", "null");
+        } else {
+            Log.v(TAG+"myToken", a.toString());
+        }
+
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        // App code
+                        AccessToken a = loginResult.getAccessToken();
+                        Profile p = Profile.getCurrentProfile();
+                        Log.v(TAG + "token", a.getUserId().toString());
+                        Log.v(TAG + "name", p.getName().toString());
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
     }
 
 
@@ -91,5 +157,15 @@ public class Login extends ActionBarActivity {
             auth_token = responseObject.getString("auth_token");
             Log.v("Login success", auth_token);
         } catch(JSONException e) {}
+    }
+
+    public void facebookLogin() {
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
